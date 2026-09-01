f9b5a1c2b8c22479779eb9651468684b200adefa
Hinoki-code <iagonascimentosilva40@gmail.com>
2026-09-01 14:28:37 -0300

    interface grafica e Segurança

--- Main.py
+++ Main.py
@@ -1,32 +1,121 @@
 import fastapi
+from fastapi.security import OAuth2PasswordBearer , OAuth2PasswordRequestForm 
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+
+from pwdlib import PasswordHash 
 from pydantic import BaseModel
 from typing import Optional
 import db
+from datetime import datetime , timedelta
+import jwt 
+import os
+
+
 
 """ Comando para rodar abrindo um server: uvicorn Main:app --reload """  
 
-app = fastapi.FastAPI()
+# Autentificação de dados recebidos com pydentic
+
 
 class Task(BaseModel):
-    id: Optional[int] = None
     title: str
     description: Optional[str] =None
     completed: bool=False
     
+class User(BaseModel):
+    Nome: str
+    Email: str
+    Senha: str
+
+class UserLogin(BaseModel):
+    Email:str
+    Senha:str
+    
+         
+ChaveSecreta = "ChaveQualquerAprendendoJWT"
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+SenhaHash = PasswordHash.recommended()
+
+app = fastapi.FastAPI()
+
+async def ObterUsuarioAtual(token:str = fastapi.Depends(oauth2_scheme)):
+   infoDecript=jwt.decode(token,ChaveSecreta, algorithms=["HS256"])
+   Id_Usuario= infoDecript.get("Usuario")
+   return Id_Usuario
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+  
+# Rotas para servir as páginas HTML automaticamente pelo Uvicorn
 @app.get("/")
-def root():
-    return {"Message":"Meu FastApi esta funcionando"}
+def serve_login():
+    return FileResponse("login.html")
+
+@app.get("/register")
+def serve_register():
+    return FileResponse("register.html")
+
+@app.get("/dashboard")
+def serve_dashboard():
+    return FileResponse("dashboard.html")
+
+
+# Login e registro de dados 
+@app.post("/api/login")
+def Login(Login:UserLogin):
+    
+    info = db.Sessao.query(db.Usuario).filter_by(Email = Login.Email).first()
+    if info is None:
+        raise fastapi.HTTPException(status_code=404 , detail="Email ou Senha invalida")
+    
+    elif not SenhaHash.verify(Login.Senha , info.Senha):
+        raise fastapi.HTTPException(status_code=404 , detail="Email ou Senha invalida")
+        
+    else:
+        token = jwt.encode(
+        {"Usuario":info.Id_usuario , "exp": datetime.utcnow() + timedelta(minutes=30)},
+        ChaveSecreta , algorithm="HS256" )
+        
+        return {"access_token": token}
+
+
+
+@app.post("/UserRegister")
+def Registrar(user:User):
+    db.Usuario.Contrutor(user.Nome , user.Email , senha=user.Senha)  
+    return "Registrado com sucesso"
+
+
+#________________________________________________________________________________________________________________________________________
+
+# Rotas
+
+
+@app.post("/tasks")
+async def Create_tasks(task:Task , Id_Usuario:int = fastapi.Depends(ObterUsuarioAtual)):
+    db.Tarefa.Contrutor(Id_Usuario , task.title , task.description , task.completed)
+    return task
+    
 
 
 @app.get("/tasks/All")
-async def get_All():
-    info = db.Sessao.query(db.Tarefa).all()
+async def get_All(Id_Usuario:int = fastapi.Depends(ObterUsuarioAtual)):
+    info = db.Sessao.query(db.Tarefa).filter_by(Id_Usuario=Id_Usuario).all()
     return info
 
     
 @app.get("/tasks/{task_id}")
-async def Get_taskid(task_id:int):  
-    info = db.Sessao.query(db.Tarefa).filter_by(Id = task_id).first()
+async def Get_taskid(task_id:int , Id_Usuario:int = fastapi.Depends(ObterUsuarioAtual)):  
+    info = db.Sessao.query(db.Tarefa).filter_by(Id_Usuario= Id_Usuario, Id = task_id).first()
     
     if info is None:
         raise fastapi.HTTPException(status_code=404 , detail="Tarefa não encontrada no banco de dados.")
@@ -34,17 +123,10 @@ async def Get_taskid(task_id:int):
         return info
 
 
-
-@app.post("/tasks")
-async def Create_tasks(task:Task):
-    db.Tarefa.Contrutor(task.title , task.description , task.completed)
-    return task
-    
     
 @app.put("/tasks/{task_id}/Update")
-async def Update_task(task_id:int ,task:Task):
-       info = db.Sessao.query(db.Tarefa).filter_by(Id = task_id).first()
-    
+async def Update_task(task_id:int ,task:Task , Id_Usuario:int = fastapi.Depends(ObterUsuarioAtual)):
+       info = db.Sessao.query(db.Tarefa).filter_by(Id_usuario=Id_Usuario,Id = task_id).first()
        info.Titulo = task.title
        info.Descricao = task.description
        info.Completo = task.completed
@@ -56,9 +138,12 @@ async def Update_task(task_id:int ,task:Task):
    
    
 @app.delete("/tasks/{task_id}/Delete")
-async def Delet_task(task_id:int):
-    info = db.Sessao.query(db.Tarefa).filter_by(Id = task_id).first()
+async def Delet_task(task_id:int, Id_Usuario:int = fastapi.Depends(ObterUsuarioAtual)):
+    info = db.Sessao.query(db.Tarefa).filter_by( Id_Usuario=Id_Usuario , Id = task_id).first()
     db.Sessao.delete(info)
     db.Sessao.commit()
     
     return "Id deletado do banco de dados"
+
+
+
--- db.py
+++ db.py
@@ -1,28 +1,55 @@
 import sqlalchemy as db 
 from sqlalchemy.orm import declarative_base , sessionmaker
+from pwdlib import PasswordHash
 
+
+
+
+# Configurar banco de dados
 Engine = db.create_engine("sqlite:///Dados.db")
 Base = declarative_base()
 TempSessao = sessionmaker(Engine)
 Sessao = TempSessao()
+SenhaHash = PasswordHash.recommended()
+  
+# Criar Tabelas
+class Usuario(Base):
+    __tablename__="Usuarios"
+    
+    Id_usuario = db.Column(db.Integer , primary_key=True , autoincrement=True)
+    Nome = db.Column(db.String(100), nullable=False)
+    Email = db.Column(db.String(100), nullable=False)
+    Senha = db.Column(db.String(255), nullable=False)
 
+    
+    @classmethod
+    def Contrutor(Cs ,Nome ,Email ,senha ):
+        
+        Hash = SenhaHash.hash(senha)
+    
+        Valores = Cs(Nome=Nome ,Email=Email , Senha=Hash)
+        Sessao.add(Valores)
+        Sessao.commit()
+        Sessao.close()
 
 class Tarefa(Base):
     __tablename__= "Tarefas"
     
-    Id =  db.Column(db.Integer, primary_key=  True , autoincrement= True )
-    Titulo = db.Column(db.String(60), nullable=  False)
+    Id =  db.Column(db.Integer, primary_key= True , autoincrement= True )
+    Id_Usuario = db.Column(db.Integer , db.ForeignKey(Usuario.Id_usuario), nullable=False)
+    Titulo = db.Column(db.String(100), nullable=  False)
     Descricao = db.Column (db.Text , nullable=False)
     Completo = db.Column(db.Boolean , default= False)
     
     @classmethod
-    def Contrutor(Cs , Titulo , Descricao , Completo):
-        Valores = Cs(Titulo=Titulo ,Descricao=Descricao ,Completo=Completo)
+    def Contrutor(Cs ,Id_Usuario, Titulo , Descricao , Completo):
+        Valores = Cs( Id_Usuario=Id_Usuario,Titulo=Titulo ,Descricao=Descricao ,Completo=Completo)
         Sessao.add(Valores)
         Sessao.commit()
         Sessao.close()
     
 
+#Gerar o banco 
 Base.metadata.create_all(Engine)
 
 
